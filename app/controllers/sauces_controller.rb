e2b6b97c2b0888df2a55ceb6d4ff80d945616d2a
Sam Skinner <samskinner@Sams-MacBook-Pro.local>
2022-08-02 13:51:27 -0400

    Created erb template and updated the routes to have :new and :create, and updated controller

--- app/controllers/sauces_controller.rb
+++ app/controllers/sauces_controller.rb
@@ -2,4 +2,24 @@ class SaucesController < ApplicationController
   def index
     @sauces = Sauce.all
   end
+
+  def new
+    @sauce = Sauce.new
+  end
+
+  def create
+    @sauce = Sauce.new(sauce_params)
+
+    
+
+
+  end
+
+
+  private
+
+  def sauce_params
+    params.require(:sauce).permit[:name, :brand, :image_url, :description]
+  end
+
 end
