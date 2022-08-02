016a9783de3a8b4f7e823ff57a958ef1b60cd6c6
Sam Skinner <samskinner@Sams-MacBook-Pro.local>
2022-08-02 17:15:11 -0400

    created new sauce form erb, added relevant paths to the routes.rb and sauces controller,  added url validation to the image_url column on the model level, added styling to the top bar.

--- app/controllers/sauces_controller.rb
+++ app/controllers/sauces_controller.rb
@@ -10,16 +10,18 @@ class SaucesController < ApplicationController
   def create
     @sauce = Sauce.new(sauce_params)
 
-    
-
-
+    if @sauce.save
+      flash[:notice] = "You successfully added that sauce."
+      redirect_to sauces_path
+    else
+      flash.now[:error] = @sauce.errors.full_messages.to_sentence 
+      render :new
+    end
   end
-
 
   private
 
   def sauce_params
-    params.require(:sauce).permit[:name, :brand, :image_url, :description]
+    params.require(:sauce).permit(:name, :brand, :image_url, :description)
   end
-
 end
--- app/models/sauce.rb
+++ app/models/sauce.rb
@@ -1,7 +1,7 @@
 class Sauce < ApplicationRecord
   validates :name, presence: true
   validates :brand, presence: true
-  validates :image_url, presence: true
+  validates :image_url, presence: true, url: true
 
   validates :name, uniqueness: { scope: :brand }
 end
