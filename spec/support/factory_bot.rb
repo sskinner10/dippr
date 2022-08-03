71f7ee8ac06d088c52c2969f70a3bf3174b11d9c
Nicholas Adams <nicholasadams@Nicholass-MacBook-Pro.local>
2022-08-03 15:19:33 -0400

    removed unicorns for merge

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -6,6 +6,7 @@ FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
     dippr_handle { 'BigDipper' }
+    # avatar { "s3://dippr-development/uploads/Mesa-Unicorn-Dominaria-MtG-Art.jpeg" }
     avatar { fixture_file_upload("#{Rails.root}/spec/support/images/unicorn.jpeg", "image/jpeg") }
     password { 'password' }
     password_confirmation { 'password' }
