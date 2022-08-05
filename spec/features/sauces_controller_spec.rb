c056b766fc7aa4ad71290882b4e4484a88f6a52b
Sam Skinner <samskinner@Sams-MacBook-Pro.local>
2022-08-05 14:54:04 -0400

    made react user show page made appropriate routes and tests, modified previous tests to be dynamic

--- spec/features/sauces_controller_spec.rb
+++ spec/features/sauces_controller_spec.rb
@@ -11,10 +11,10 @@ RSpec.describe Api::V1::SaucesController, type: :controller do
       expect(response.status).to eq 200
       expect(response.content_type).to eq("application/json")
 
-      expect(returned_json['name']).to eq "Ketchup"
-      expect(returned_json['brand']).to eq "Heinz"
-      expect(returned_json['image_url']).to eq "https://m.media-amazon.com/images/I/71szc59Y0aL._SL1500_.jpg"
-      expect(returned_json['description']).to eq "a tasty, tomato based sauce"
+      expect(returned_json['name']).to eq ketchup.name
+      expect(returned_json['brand']).to eq ketchup.brand
+      expect(returned_json['image_url']).to eq ketchup.image_url
+      expect(returned_json['description']).to eq ketchup.description
     end 
   end 
 end 
