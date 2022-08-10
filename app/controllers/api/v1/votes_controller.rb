c89eda14c09227a8b637a80ead5aaf52f354ac9d
Sam Skinner <samskinner@Sams-MacBook-Pro.local>
2022-08-10 10:40:00 -0400

    removed some of the logic from the create method in the votes_controller.rb and placed the logic in relevant methods. Changed .where to .find_by for checking an existing vote. Fixed indentation?

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -2,41 +2,13 @@ class Api::V1::VotesController < ApiController
 	before_action :authenticate_user
 
   def create
-		existing_vote = Vote.where(user_id: current_user.id, review_id: params['review_id']).first
-
+		existing_vote = Vote.find_by(user_id: current_user.id, review_id: params['review_id'])
+    
     if existing_vote
       review = Review.find(params['review_id'])
-
-      if existing_vote.vote_type === params['vote_type']
-        existing_vote.destroy
-
-        karma = review.total_karma
-        vote = { vote_type: nil }
-        payload = { vote: vote, karma: karma }
-
-        render json: payload
-      else
-        existing_vote.update(vote_type: params['vote_type'])
-
-        karma = review.total_karma
-        payload = { vote: existing_vote, karma: karma}
-
-        render json: payload
-      end
-
+      change_existing_vote(existing_vote, review)
     else
-      vote_hash = vote_params.merge!({user_id: current_user.id})
-      vote = Vote.new(vote_hash)
-
-      if vote.save
-        review = Review.find(vote.review_id)
-        karma = review.total_karma
-        payload = { vote: vote, karma: karma }
-
-        render json: payload
-      else
-        render json: {error: "An error occurred."}
-      end
+      create_new_vote
     end
   end
 
@@ -50,5 +22,39 @@ class Api::V1::VotesController < ApiController
 
   def vote_params
     params.require(:vote).permit(:vote_type, :review_id)
+  end
+
+  def change_existing_vote(existing_vote, review)
+    if existing_vote.vote_type === params['vote_type']
+      existing_vote.destroy
+
+      karma = review.total_karma
+      vote = { vote_type: nil }
+      payload = { vote: vote, karma: karma }
+
+      render json: payload
+    else
+      existing_vote.update(vote_type: params['vote_type'])
+
+      karma = review.total_karma
+      payload = { vote: existing_vote, karma: karma}
+
+      render json: payload
+    end
+  end
+
+  def create_new_vote 
+    vote_hash = vote_params.merge!({user_id: current_user.id})
+    vote = Vote.new(vote_hash)
+
+    if vote.save
+      review = Review.find(vote.review_id)
+      karma = review.total_karma
+      payload = { vote: vote, karma: karma }
+
+      render json: payload
+    else
+      render json: {error: "An error occurred."}
+    end
   end
 end
